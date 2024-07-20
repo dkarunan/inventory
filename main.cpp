d03be82d5d128338d569a0bb99faf10897fc476b
dkarunan <rcd.amila@gmail.com>
2024-07-20 23:48:09 +0530

    Initial implementation

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Student.h"
+#include <iostream>
+
+int main() 
+{
+    // Create a student object
+    Student student1("S001", "Alice", "Mechanical Engineering", 3, "alice@example.com");
+
+    // Display student information
+    student1.display();
+
+    return 0;
+}
